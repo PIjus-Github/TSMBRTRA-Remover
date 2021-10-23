7728049d4c7718413a5948b3be3d62b9f90ea4e7
PIjus-Github <92991997+PIjus-Github@users.noreply.github.com>
2021-10-23 15:09:00 +0300

    final changes

--- tsmbtra-remover.js
+++ tsmbtra-remover.js
@@ -2,7 +2,7 @@
 // @name         TSMBRTRA Remover
 // @namespace    https://github.com/PIjus-Github
 // @version      1.0
-// @description  Removes the "This search may be relevant to recent activity" message when searching on Google.
+// @description  Removes the "This search may be relevant to recent activity" message when searching on Google
 // @author       PIjus
 // @match        https://*.google.com/*
 // @grant        https://*.google.com/*
